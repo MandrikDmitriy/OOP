116841885c581aab8dcf541b8bb8c4dd1ef8eb64
Dmitriy <mandrik_dem@tut.by>
2024-03-25 14:06:20 +0300

    task 2.1.3 completed

---
+++ 2_Access_modes_properties_descriptors/2_1_Access_modes_public_privet_protected/task_2.1.3.py
@@ -0,0 +1,20 @@
+class Clock:
+    def __init__(self, tm=0):
+        self.__time = 0
+        if self.__check_time(tm):
+            self.__time = tm
+
+    @classmethod
+    def __check_time(cls, tm):
+        return type(tm) == int and 0 <= tm < 100000
+
+    def set_time(self, tm):
+        if self.__check_time(tm):
+            self.__time = tm
+
+    def get_time(self):
+        return self.__time
+
+
+clock = Clock(4530)
+print(clock.get_time())
