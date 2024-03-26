97f120f8ab473e51160492ca9b14810b936b6b47
Dmitriy <mandrik_dem@tut.by>
2024-03-26 10:34:47 +0300

    task 2.1.8 completed

--- 2_Access_modes_properties_descriptors/2_1_Access_modes_public_privet_protected/task_2.1.8.py
+++ 2_Access_modes_properties_descriptors/2_1_Access_modes_public_privet_protected/task_2.1.8.py
@@ -1,6 +1,5 @@
 class Point:
-    def __init__(self, x, y):
-        self.__x = self.__y = 0
+    def __init__(self, x=0, y=0):
         if self.__check_point(x, y):
             self.__x = x
             self.__y = y
@@ -13,9 +12,28 @@ class Point:
         return type(x) in (int, float) and type(y) in (int, float)
 
 class Rectangle:
-    def __init__(self, sp, ep, *args):
-        pass
+    def __init__(self, x1, y1, x2=None, y2=None):
+        self.__sp = self.__ep = None
+        if isinstance(x1, Point) and isinstance(y1, Point):
+            self.__sp = x1
+            self.__ep = y1
+        elif all(map(lambda x: type(x) in (int, float), (x1, y1, x2, y2))):
+            self.__sp = Point(x1, y1)
+            self.__ep = Point(x2, y2)
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return  self.__sp, self.__ep
+
+    def drow(self):
+        print(f'Прямоугольник с координатами: {self.__sp.get_coords()}{self.__ep.get_coords()}')
 
 
-
-rect = Rectangle(1, 2, 3,4)
+rect = Rectangle(0, 1, 20, 34)
+sp = Point(8, 9)
+ep = Point(10, 11)
+rect.set_coords(sp, ep)
+rect.drow()
