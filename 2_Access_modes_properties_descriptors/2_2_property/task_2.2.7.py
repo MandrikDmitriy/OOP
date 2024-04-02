bfcb138813cb2ffeef1ce5d88c089bb17afc9dfa
Dmitriy <mandrik_dem@tut.by>
2024-04-02 14:21:02 +0300

    task 2.2.7 completed

---
+++ 2_Access_modes_properties_descriptors/2_2_property/task_2.2.7.py
@@ -0,0 +1,40 @@
+class RadiusVector2D:
+    MIN_COORD = -100
+    MAX_COORD = 1024
+
+    def __init__(self, x=0, y=0):
+        self.__x = self.__y = 0
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def __is_verify(cls, value):
+        return type(value) in (int, float) and cls.MIN_COORD <= value <= cls.MAX_COORD
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if self.__is_verify(value):
+            self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if self.__is_verify(value):
+            self.__y = value
+
+    @staticmethod
+    def norm2(vector):
+        return vector.x * vector.x + vector.y * vector.y
+
+
+v1 = RadiusVector2D()
+v2 = RadiusVector2D(1)
+v3 = RadiusVector2D(1, 2)
+print(RadiusVector2D.norm2(v3))
