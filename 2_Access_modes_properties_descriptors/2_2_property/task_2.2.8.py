eeabd75dd9b8d76400d91e45c0d84a67731582d5
Dmitriy <mandrik_dem@tut.by>
2024-04-05 11:45:06 +0300

    task 2.2.8 completed

--- 2_Access_modes_properties_descriptors/2_2_property/task_2.2.8.py
+++ 2_Access_modes_properties_descriptors/2_2_property/task_2.2.8.py
@@ -24,8 +24,53 @@ class TreeObj:
 class DecisionTree:
     @classmethod
     def predict(cls, root, x):
-        pass
+        obj = root
+        while obj:
+            obj_next = cls.get_next(obj, x)
+            if obj_next is None:
+                break
+            obj = obj_next
+        return obj.value
+
+    @classmethod
+    def get_next(cls, obj, x):
+        if x[obj.indx] == 1:
+            return obj.left
+        return obj.right
+
+        # next_root = root
+        # if x[0] == 1:
+        #     for i in [0, 1]:
+        #         if x[i] == 1:
+        #             next_root = next_root.left
+        #         else:
+        #             next_root = next_root.right
+        # else:
+        #     for i in [0, 2]:
+        #         if x[i] == 1:
+        #             next_root = next_root.left
+        #         else:
+        #             next_root = next_root.right
+        # return next_root.value
 
     @classmethod
     def add_obj(cls, obj, node=None, left=True):
-        pass
+        if node:
+            if left:
+                node.left = obj
+            else:
+                node.right = obj
+        return obj
+
+
+root = DecisionTree.add_obj(TreeObj(0))
+v_11 = DecisionTree.add_obj(TreeObj(1), root)
+v_12 = DecisionTree.add_obj(TreeObj(2), root, False)
+DecisionTree.add_obj(TreeObj(-1, 'will be programmer'), v_11)
+DecisionTree.add_obj(TreeObj(-1, 'will be coder'), v_11, False)
+DecisionTree.add_obj(TreeObj(-1, 'not all is lost'), v_12)
+DecisionTree.add_obj(TreeObj(-1, 'you loser'), v_12, False)
+
+x = [0, 1, 1]
+res = DecisionTree.predict(root, x)
+print(res)
