60c102d7323843cb41d3a49ec849a0f5a51cb43e
Dmitriy <mandrik_dem@tut.by>
2024-04-10 13:30:26 +0300

    task 3.1.7 completed

---
+++ 3_Magic_class_methods/3_1_setattr_methods/task_3.1.7.py
@@ -0,0 +1,37 @@
+class SmartPhone:
+    def __init__(self, model):
+        self.model = model
+        self.apps = []
+
+    def add_app(self, app):
+        if len(tuple(filter(lambda x: type(x) is type(app), self.apps))) == 0:
+            self.apps.append(app)
+
+    def remove_app(self, app):
+        if app in self.apps:
+            self.apps.remove(app)
+
+
+class AppVK:
+    def __init__(self):
+        self.name = 'VKontakte'
+
+
+class AppYouTube:
+    def __init__(self, memory_max):
+        self.name = 'YouTube'
+        self.memory_max = memory_max
+
+
+class AppPhone:
+    def __init__(self, phone_list):
+        self.name = 'Phone'
+        self.phone_list = phone_list
+
+
+sm = SmartPhone('Honor 1.0')
+sm.add_app(AppVK())
+sm.add_app(AppVK())
+sm.add_app(AppYouTube(2048))
+for a in sm.apps:
+    print(a.name)
