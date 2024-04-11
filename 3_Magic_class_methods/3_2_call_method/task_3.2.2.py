6660f9d30e7e69b909ddaf93af93b4847f378d5a
Dmitriy <mandrik_dem@tut.by>
2024-04-11 14:15:21 +0300

    task 3.2.2 completed

---
+++ 3_Magic_class_methods/3_2_call_method/task_3.2.2.py
@@ -0,0 +1,19 @@
+from random import randint
+
+
+class RandomPassword:
+    def __init__(self, psw_chars, min_length, max_length):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+        n = randint(self.min_length, self.max_length)
+        len_chars = len(self.psw_chars)
+        return ''.join(self.psw_chars[randint(0, len_chars - 1)] for _ in range(n))
+
+
+rnd = RandomPassword("qwertyuiopasdfghjklzxcvbnm!@#$%&*", 5, 20)
+psw = rnd()
+lst_pass = [rnd() for _ in range(3)]
+print(lst_pass)
